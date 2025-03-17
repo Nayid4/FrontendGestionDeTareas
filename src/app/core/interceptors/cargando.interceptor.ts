7b997f2862c6c1d86f081c0ac51ca388f8e260c9
nayid2004@gmail.com <jmesquivel@unicesar.edu.co>
2025-03-17 16:12:47 -0500

    Eliminacion de archivos innecesarios

--- src/app/core/interceptors/cargando.interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const cargandoInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     //apiUrlBase: 'https://localhost:44358',
     apiUrlBase: 'http://localhost:8080',
-    //apiUrlBase: 'https://gestion-de-tareas.azurewebsites.net',
     production: false
 };
