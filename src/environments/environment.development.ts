31ac91fa49d0ac631a3cae4364c22c5cc6796e35
Nayid4 <nayid2004@gmail.com>
2026-04-13 18:44:13 -0500

    Ajuste de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
     //apiUrlBase: 'https://localhost:44358',
-    //apiUrlBase: 'http://localhost:8080',
-    apiUrlBase: 'https://gestion-de-tareas.azurewebsites.net',
+    //apiUrlBase: 'http://localhost:8081',
+    //apiUrlBase: 'https://gestion-de-tareas.azurewebsites.net',
+    apiUrlBase: 'http//:internal-mini-proyecto-application-alb-886082670.us-east-1.elb.amazonaws.com',
     production: false
 };
